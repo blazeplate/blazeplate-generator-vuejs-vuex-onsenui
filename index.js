72c60c015531e64764e374563b98699e88cc5e14
Alexander Schwartzberg <aeksco@gmail.com>
2018-07-31 23:05:23 -0400

    Wired up root generator with ToDo List example

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // TODO - this should be `blazeplate-generator`
-let Blazeplate = require('blazeplate_generator/generators/app')
+const generator = require('./generator')
 
-// Invoke Blazeplate directly with the ToDo List example
-new Blazeplate({
+// Invoke generator directly with the ToDo List example
+new generator({
   appconfig: require('blazeplate_generator/examples/todo-list.json'),
   buildId: 'app_5acfeea85535afdb753d55f7'
 }).write()
